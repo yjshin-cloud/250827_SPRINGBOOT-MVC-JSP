82872169a053c8c4f75563cb316c1bb90c36f3cf
yjshin.cloud <yjshin.cloud@gmail.com>
2025-08-27 17:33:03 +0900

    HelloController Code 수정

--- src/main/java/com/example/springmvcjsp/controller/HelloController.java
+++ src/main/java/com/example/springmvcjsp/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.springmvcjsp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 // @RequestGetMapping("/")
 public class HelloController {
     @GetMapping("/greeting")
-    public String greet() {
+    public String greet(Model model) { // 일종의 의존성 주입
+        // key, value
+        model.addAttribute("username", "Tiger");
+        model.addAttribute("message", "9월에는 공휴일이 없어요 ㅠㅠ");
         return "greet";
-        // web-app/WEB-INF/views + {...} + .jsp
+        // web-app/WEB-INF/views/ + {...} + .jsp
     }
 }
